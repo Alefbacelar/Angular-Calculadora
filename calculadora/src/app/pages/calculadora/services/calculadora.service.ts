49a12d6834372d12feee19eda33bc7a0926756f6
ALEF BACELAR <74435383+Alefbacelar@users.noreply.github.com>
2022-12-21 14:50:42 -0300

    Done Calculadora

--- calculadora/src/app/pages/calculadora/services/calculadora.service.ts
+++ calculadora/src/app/pages/calculadora/services/calculadora.service.ts
@@ -5,5 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
-  constructor() { }
+  //DEFINE AS CONSTANTES UTILIZADASS PARA INDENTIFICAR
+  //AS OPERAÇÕES DE CÁLCULO
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
+  constructor() {}
+
+  calcular(num1: number, num2: number, operacao: string): number {
+  	let resultado: number; // armazena o resultado da operação
+
+  	switch(operacao) {
+  	  case CalculadoraService.SOMA:
+  	    resultado = num1 + num2;
+  		break;
+  	  case CalculadoraService.SUBTRACAO:
+  	    resultado = num1 - num2;
+  		break;
+  	  case CalculadoraService.DIVISAO:
+  	    resultado = num1 / num2;
+  		break;
+  	  case CalculadoraService.MULTIPLICACAO:
+  	    resultado = num1 * num2;
+  		break;
+  	  default:
+  	    resultado = 0;
+  	}
+
+  	return resultado;
+  }
 }
